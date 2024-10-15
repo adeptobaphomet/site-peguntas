e363d7a6c6ce25c233e55cb1aee179fdcd2a8a66
adeptobaphomet <146438047+adeptobaphomet@users.noreply.github.com>
2024-10-15 10:43:21 -0300

    Add files via upload

---
+++ Site Perguntas/perguntas.js
@@ -0,0 +1,11 @@
+criaCartao(
+    'anime',
+'quantas temporadas tem jojos bizarre adventure?',
+    'Sendo dividido em nove partes únicas, cada uma seguindo a história de um descendente da família Joestar, que inevitavelmente tem um nome que pode ser abreviado para o titular "JoJo". As primeiras seis partes da série ocorrem em uma única continuidade, enquanto as partes 7, 8 e 9 ocorrem em uma continuidade alternativa.'
+)
+criaCartao(
+    'protagonistas de jojo',
+    'quais são os protagonistas de jojo e suas respectivas temporadas',
+
+
+    'Jonathan Joestar	Phantom Blood, Joseph Joestar	Battle Tendency, Josuke Higashikata	Diamond is Unbreakable, Giorno Giovanna 	Vento Aureo golden Wind, Jolyne Cujoh	Stone Ocean, Johnny Joestar	Steel Ball Run, Josuke Higashikata Gappy	JoJolion, Jodio Joestar	The JOJOLands')
